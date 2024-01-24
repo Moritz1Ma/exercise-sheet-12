414d35f0bca1f306c0167040076d0840229abe06
nori <norajasharaj631@gmail.com>
2024-01-24 21:31:32 +0100

    fixed the ugly code(still ugly tho)

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet12/homework/cleancode/SomethingWithCalendars.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet12/homework/cleancode/SomethingWithCalendars.java
@@ -7,27 +7,35 @@ import java.util.Scanner;
 
 public class SomethingWithCalendars {
 
-	static String[] weekDays = new String[] {"Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri", "Satur"};
+    static String[] weekDays = new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-	static void setCalendarDate(final Calendar cal, final String date, final String sep) {
-		final String[] fs = date.split(sep);
-		cal.set(parseInt(fs[0]), parseInt(fs[1]) - 1, parseInt(fs[2]));
-	}
+    static void setCalendarDate(final Calendar calendar, final String date, final String separator) {
+        final String[] finalDate = date.split(separator);
+        calendar.set(parseInt(finalDate[0]), parseInt(finalDate[1]) - 1, parseInt(finalDate[2]));
+    }
 
-	public static void main(final String[] args) {
-		final Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the input for SomethingWithCalendars: (press enter to confirm)");
-		final String input = scanner.nextLine();
-		final Calendar cal = Calendar.getInstance();
-		if (input.indexOf("-") >= 0) {
-			setCalendarDate(cal, input, "-");
-		} else if (input.indexOf("/") >= 0) {
-			setCalendarDate(cal, input, "/");
-		} else if (input.indexOf(".") >= 0) {
-			final String[] fs = input.split(".");
-			cal.set(parseInt(fs[0]), parseInt(fs[1]) - 1, parseInt(fs[2]));
-		}
-		final int wd = cal.get(Calendar.DAY_OF_WEEK);
-		System.out.printf("%sday\n", weekDays[wd - 1]);
-	}
+
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the input for SomethingWithCalendars: (press enter to confirm)");
+        final String input = scanner.nextLine();
+        final Calendar calendar = Calendar.getInstance();
+        final String separator = getSeparator(input);
+        if (separator != null) {
+            setCalendarDate(calendar, input, separator);
+        }
+        final int weekday = calendar.get(Calendar.DAY_OF_WEEK);
+        System.out.printf(weekDays[weekday - 1]);
+    }
+
+    private static String getSeparator(final String input) {
+        if (input.contains("-")) {
+            return "-";
+        } else if (input.contains("/")) {
+            return "/";
+        } else if (input.contains(".")) {
+            return "//.";
+        }
+        return null;
+    }
 }
